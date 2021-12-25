cd7548f84993ed76d6c7d9c8b65c84e2e946062c
HalimSD <maingateway1@gmail.com>
2021-12-26 00:11:06 +0100

    adding the Normalization class and a function to change the Sequantial model to fit the usecase

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -4,9 +4,10 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from PIL import Image
 import matplotlib.pyplot as plt
+import torchvision.models as models
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 image_size = 512 if torch.cuda.is_available() else 128
 
@@ -67,3 +68,64 @@ class StyleLoss(nn.Module):
     G = gram_matrix(input)
     self.loss = nn.functional.mse(G, self.target)
     return input
+
+cnn = models.vgg16(pretrained=True).features.to(device).eval()
+cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+class Normalization(nn.Module):
+  def __init__(self, mean, std):
+    super().__init__(Normalization, self)
+    self.mean = torch.tensor(mean).view(-1,1,1)
+    self.std = torch.tensor(std).view(-1,1,1)
+
+  def forward(self, img):
+    return (img - self.mean) / self.std
+
+content_layers_default = ['conv_4']
+style_layer_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+def get_style_content_and_losses(cnn, normalization_mean, normalization_std, 
+                                 style_image, content_image, 
+                                 content_layers = content_layers_default, style_layers = style_layer_default):
+  
+  normalization = Normalization(normalization_mean, normalization_std).to(device)
+  content_losses = []
+  style_losses = []
+  model = nn.Sequential(normalization)
+
+  i = 0
+  for layer in cnn.children():
+    if isinstance(layer, nn.Conv2d):
+      i += 1
+      name = 'conv_{}'.format(i)
+    elif isinstance(layer, nn.ReLU):
+      name = 'relu_{}'.format(i)
+      layer = nn.ReLU(inplace=False)
+    elif isinstance|(layer, nn.MaxPool2d):
+      name = 'pool_{}'.formate(i)
+    elif isinstance(layer, nn.BatchNorm2d):
+      name = 'bn_{}'.format(i)
+    else: 
+      raise RuntimeError('unrecognized layer: {}'.format(layer.__class__.__name__))
+    
+    model.add_module(name, layer)
+
+    if name in content_layers:
+      target = model(content_image).detach()
+      content_loss = ContentLoss(target=target)
+      model.add_module("content_loss_{}".format(i), content_loss)
+      content_losses.append(content_loss)
+
+    if name in style_layers:
+      target_features = model(style_image).detach()
+      style_loss = StyleLoss(target_features=target_features)
+      model.add_module("style_loss_{}".format(i), style_loss)
+      style_losses.append(style_loss)
+  
+  for i in range(len(model) - 1, -1, -1):
+    if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+      break
+  
+  model = model[:(i+1)]
+  return model, style_losses, content_losses
