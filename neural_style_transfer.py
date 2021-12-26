5eea295f48b4be457cec5a45c2ab66a22e061777
HalimSD <maingateway1@gmail.com>
2021-12-26 01:59:56 +0100

    bug fixes and model testing

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -6,30 +6,31 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import torchvision.models as models
 
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 image_size = 512 if torch.cuda.is_available() else 128
 
-loader = transforms.Compose([
-                            transforms.Resize(image_size),
-                            transforms.ToTensor()
-])
+loader = transforms.Compose([transforms.Resize(image_size), transforms.ToTensor()])
+
 
 def image_loader(image_name):
     image = Image.open(image_name)
     image = loader(image).unsqueeze(0)
     return image.to(device, torch.float)
 
-style_image = image_loader('./images/picasso.jpg')
-content_image = image_loader('./images/dancing.jpg')
 
-assert style_image.size() == content_image.size(), "import style and content images of the same size"
+style_image = image_loader("./images/picasso.jpg")
+content_image = image_loader("./images/dancing.jpg")
 
-unloader = transforms.ToPILImage() # reconvert the tensor image to pil to display it
-plt.ion() # turn the interactive mode on
+assert (
+    style_image.size() == content_image.size()
+), "import style and content images of the same size"
 
-def im_show(tensor, title=None):
+unloader = transforms.ToPILImage()  # reconvert the tensor image to pil to display it
+plt.ion()  # turn the interactive mode on
+
+
+def imshow(tensor, title=None):
     image = tensor.cpu().clone()
     image = image.squeeze(0)
     image = unloader(image)
@@ -38,94 +39,192 @@ def im_show(tensor, title=None):
         plt.title = title
     plt.pause(0.001)
 
-plt.figure()
-im_show(content_image, 'Content Image')
 
 plt.figure()
-im_show(style_image, 'Style Image')
+imshow(content_image, "Content Image")
+
+plt.figure()
+imshow(style_image, "Style Image")
+
 
 class ContentLoss(nn.Module):
-  def __init__(self, target,):
-    super(ContentLoss, self).__init__()
-    self.target = target.detch()
+    def __init__(
+        self, target,
+    ):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
 
-  def forward(self, input):
-    self.loss = nn.functional.mse_loss(input, self.target)
-    return input
+    def forward(self, input):
+        self.loss = nn.functional.mse_loss(input, self.target)
+        return input
+
 
 def gram_matrix(input):
-  batch_size, feature_maps_numbers, dim1, dim2 = input.size()
-  features = input.view(batch_size * feature_maps_numbers, dim1 * dim2)
-  G = torch.mm(features, features.t())
-  return G.div(batch_size, feature_maps_numbers, dim1, dim2)
+    batch_size, feature_maps_numbers, dim1, dim2 = input.size()
+    features = input.view(batch_size * feature_maps_numbers, dim1 * dim2)
+    G = torch.mm(features, features.t())
+    return G.div(batch_size * feature_maps_numbers * dim1 * dim2)
+
 
 class StyleLoss(nn.Module):
-  def __init__(self, target_features):
-    super(StyleLoss, self).__init__()
-    self.target = gram_matrix(target_features).detach()
+    def __init__(self, target_features):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_features).detach()
 
-  def forward(self, input):
-    G = gram_matrix(input)
-    self.loss = nn.functional.mse(G, self.target)
-    return input
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = nn.functional.mse_loss(G, self.target)
+        return input
+
 
 cnn = models.vgg16(pretrained=True).features.to(device).eval()
 cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
 cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 
+
 class Normalization(nn.Module):
-  def __init__(self, mean, std):
-    super().__init__(Normalization, self)
-    self.mean = torch.tensor(mean).view(-1,1,1)
-    self.std = torch.tensor(std).view(-1,1,1)
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
 
-  def forward(self, img):
-    return (img - self.mean) / self.std
+    def forward(self, img):
+        return (img - self.mean) / self.std
 
-content_layers_default = ['conv_4']
-style_layer_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
 
-def get_style_content_and_losses(cnn, normalization_mean, normalization_std, 
-                                 style_image, content_image, 
-                                 content_layers = content_layers_default, style_layers = style_layer_default):
-  
-  normalization = Normalization(normalization_mean, normalization_std).to(device)
-  content_losses = []
-  style_losses = []
-  model = nn.Sequential(normalization)
+content_layers_default = ["conv_4"]
+style_layer_default = ["conv_1", "conv_2", "conv_3", "conv_4", "conv_5"]
 
-  i = 0
-  for layer in cnn.children():
-    if isinstance(layer, nn.Conv2d):
-      i += 1
-      name = 'conv_{}'.format(i)
-    elif isinstance(layer, nn.ReLU):
-      name = 'relu_{}'.format(i)
-      layer = nn.ReLU(inplace=False)
-    elif isinstance|(layer, nn.MaxPool2d):
-      name = 'pool_{}'.formate(i)
-    elif isinstance(layer, nn.BatchNorm2d):
-      name = 'bn_{}'.format(i)
-    else: 
-      raise RuntimeError('unrecognized layer: {}'.format(layer.__class__.__name__))
-    
-    model.add_module(name, layer)
 
-    if name in content_layers:
-      target = model(content_image).detach()
-      content_loss = ContentLoss(target=target)
-      model.add_module("content_loss_{}".format(i), content_loss)
-      content_losses.append(content_loss)
+def get_style_model_and_losses(
+    cnn,
+    normalization_mean,
+    normalization_std,
+    style_image,
+    content_image,
+    content_layers=content_layers_default,
+    style_layers=style_layer_default,
+):
 
-    if name in style_layers:
-      target_features = model(style_image).detach()
-      style_loss = StyleLoss(target_features=target_features)
-      model.add_module("style_loss_{}".format(i), style_loss)
-      style_losses.append(style_loss)
-  
-  for i in range(len(model) - 1, -1, -1):
-    if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
-      break
-  
-  model = model[:(i+1)]
-  return model, style_losses, content_losses
+    normalization = Normalization(normalization_mean, normalization_std).to(device)
+    content_losses = []
+    style_losses = []
+    model = nn.Sequential(normalization)
+
+    i = 0
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = "conv_{}".format(i)
+        elif isinstance(layer, nn.ReLU):
+            name = "relu_{}".format(i)
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = "pool_{}".format(i)
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = "bn_{}".format(i)
+        else:
+            raise RuntimeError(
+                "unrecognized layer: {}".format(layer.__class__.__name__)
+            )
+
+        model.add_module(name, layer)
+
+        if name in content_layers:
+            target = model(content_image).detach()
+            content_loss = ContentLoss(target=target)
+            model.add_module("content_loss_{}".format(i), content_loss)
+            content_losses.append(content_loss)
+
+        if name in style_layers:
+            target_features = model(style_image).detach()
+            style_loss = StyleLoss(target_features=target_features)
+            model.add_module("style_loss_{}".format(i), style_loss)
+            style_losses.append(style_loss)
+
+    for i in range(len(model) - 1, -1, -1):
+        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+            break
+
+    model = model[: (i + 1)]
+    return model, style_losses, content_losses
+
+input_image = content_image.clone()
+
+def get_input_optimizer(input_image):
+    optimizer = torch.optim.LBFGS([input_image])
+    return optimizer
+
+def style_transfer(
+    cnn,
+    normalization_mean,
+    normalization_std,
+    content_img,
+    style_img,
+    input_img,
+    num_steps=300,
+    style_weight=1000000,
+    content_weight=1,
+):
+
+    print("Building the style transfer model..")
+    model, style_losses, content_losses = get_style_model_and_losses(
+        cnn, normalization_mean, normalization_std, style_img, content_img
+    )
+
+    input_img.requires_grad_(True)
+    model.requires_grad_(False)
+
+    optimizer = get_input_optimizer(input_img)
+
+    print("Optimizing..")
+    run = [0]
+    while run[0] <= num_steps:
+
+        def closure():
+            with torch.no_grad():
+                input_img.clamp_(0, 1)
+
+            optimizer.zero_grad()
+            model(input_img)
+            style_score = 0
+            content_score = 0
+
+            for sl in style_losses:
+                style_score += sl.loss
+            for cl in content_losses:
+                content_score += cl.loss
+
+            style_score *= style_weight
+            content_score *= content_weight
+
+            loss = style_score + content_score
+            loss.backward()
+
+            run[0] += 1
+            if run[0] % 50 == 0:
+                print("run {}:".format(run))
+                print(
+                    "Style Loss : {:4f} Content Loss: {:4f}".format(
+                        style_score.item(), content_score.item()
+                    )
+                )
+                print()
+
+            return style_score + content_score
+
+        optimizer.step(closure)
+
+    with torch.no_grad():
+        input_img.clamp_(0, 1)
+
+    return input_img
+
+output = style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std,
+                            content_image, style_image, input_image)
+
+plt.figure()
+imshow(output)
+
+plt.ioff()
+plt.show()
